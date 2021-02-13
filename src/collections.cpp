96c926ab348b36b182d728cd6783e8fac6c676d1
amrbashir <48618675+amrbashir@users.noreply.github.com>
2021-02-13 20:16:55 +0200

    feat: close the hidden parent window when the visible one closes

--- src/collections.cpp
+++ src/collections.cpp
@@ -4,22 +4,33 @@
 #include <Windows.h>
 #include <winrt/Windows.Foundation.Collections.h>
 using namespace winrt;
-void *ivector(const char *js)
-{
+void* ivector(const char* js) {
     auto x = winrt::single_threaded_vector<hstring>({winrt::to_hstring(js)});
 
-    return reinterpret_cast<void *>(winrt::detach_abi(x));
+    return reinterpret_cast<void*>(winrt::detach_abi(x));
 }
 
-void SkipTaskbar(HWND window){
+HWND hidden_parent;
+LRESULT CALLBACK windowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_DESTROY:
+            CloseWindow(hidden_parent);
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
 
+void SkipTaskbar(HWND window) {
     LPTSTR className;
     GetClassName(window, className, 100);
 
-    const HWND parent = CreateWindowEx(0, className, (LPCSTR) "Hidden Parent", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    hidden_parent = CreateWindowEx(0, className, (LPCSTR) "Hidden Parent", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
-    SetWindowLongPtr(window, GWLP_HWNDPARENT, (LONG_PTR)parent);
+    SetWindowLongPtr(window, GWLP_HWNDPARENT, (LONG_PTR)hidden_parent);
     SetWindowLong(window, GWL_EXSTYLE,
                   GetWindowLong(window, GWL_EXSTYLE) & ~WS_EX_APPWINDOW & ~WS_EX_TOOLWINDOW);
-
+    SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)&windowProc);
 }
