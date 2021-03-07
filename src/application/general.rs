8a6a3689a2c81d55829f91c512fe5b95b85b43bf
Amr Bashir <48618675+amrbashir@users.noreply.github.com>
2021-03-07 15:41:55 +0200

    fix: exit winit's event loop in `WindowMessage::Close` if necessary

--- src/application/general.rs
+++ src/application/general.rs
@@ -202,6 +202,9 @@ impl App for InnerApplication {
                                 WindowMessage::Hide => window.set_visible(false),
                                 WindowMessage::Close => {
                                     windows.remove(&id);
+                                    if windows.is_empty() {
+                                        *control_flow = ControlFlow::Exit;
+                                    }
                                 }
                                 WindowMessage::SetDecorations(decorations) => {
                                     window.set_decorations(decorations)
