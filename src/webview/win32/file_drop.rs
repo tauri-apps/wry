fff7ecad3d012b1da368ee14be01f475b6b5c777
Ngo Iok Ui <wusyong9104@gmail.com>
2021-04-07 13:12:28 +0800

    cargo fmt

--- src/webview/win32/file_drop.rs
+++ src/webview/win32/file_drop.rs
@@ -292,14 +292,10 @@ impl IDropTarget {
     } else if get_data_result == DV_E_FORMATETC {
       // If the dropped item is not a file this error will occur.
       // In this case it is OK to return without taking further action.
-      log::warn!(
-        "Error occured while processing dropped/hovered item: item is not a file."
-      );
+      log::warn!("Error occured while processing dropped/hovered item: item is not a file.");
       return None;
     } else {
-      log::warn!(
-        "Unexpected error occured while processing dropped/hovered item."
-      );
+      log::warn!("Unexpected error occured while processing dropped/hovered item.");
       return None;
     }
   }
--- src/webview/winrt/file_drop.rs
+++ src/webview/winrt/file_drop.rs
@@ -279,14 +279,10 @@ impl DropTarget {
     } else if get_data_result.0 == SystemServices::DV_E_FORMATETC.0 {
       // If the dropped item is not a file this error will occur.
       // In this case it is OK to return without taking further action.
-      log::warn!(
-        "Error occured while processing dropped/hovered item: item is not a file."
-      );
+      log::warn!("Error occured while processing dropped/hovered item: item is not a file.");
       return None;
     } else {
-      log::warn!(
-        "Unexpected error occured while processing dropped/hovered item."
-      );
+      log::warn!("Unexpected error occured while processing dropped/hovered item.");
       return None;
     }
   }
