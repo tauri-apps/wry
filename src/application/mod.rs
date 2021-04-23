62b22a7ac65f6622b0b9d867d37f1c75da5a72e9
Yu-Wei Wu <wusyong9104@gmail.com>
2021-04-23 12:31:49 +0800

    cargo fmt

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -21,108 +21,104 @@ pub mod window;
 pub use original::*;
 #[cfg(not(target_os = "linux"))]
 mod original {
-    pub use winit::dpi;
-    pub use winit::error;
-    pub use winit::event;
-    pub use winit::event_loop;
-    pub use winit::monitor;
-    pub use winit::window;
+  pub use winit::{dpi, error, event, event_loop, monitor, window};
 
-    #[cfg(target_os = "macos")]
-    pub use winit::platform;
-    #[cfg(target_os = "windows")]
-    pub mod platform {
-        pub use winit::platform::run_return;
+  #[cfg(target_os = "macos")]
+  pub use winit::platform;
+  #[cfg(target_os = "windows")]
+  pub mod platform {
+    pub use winit::platform::run_return;
 
-        pub mod windows {
-            pub use winit::platform::windows::DeviceIdExtWindows;
-            pub use winit::platform::windows::EventLoopExtWindows;
-            pub use winit::platform::windows::IconExtWindows;
-            pub use winit::platform::windows::MonitorHandleExtWindows;
-            pub use winit::platform::windows::WindowBuilderExtWindows;
-            use winit::platform::windows::WindowExtWindows as WindowExtWindows_;
+    pub mod windows {
+      use winit::platform::windows::WindowExtWindows as WindowExtWindows_;
+      pub use winit::platform::windows::{
+        DeviceIdExtWindows, EventLoopExtWindows, IconExtWindows, MonitorHandleExtWindows,
+        WindowBuilderExtWindows,
+      };
 
-            use winit::window::{Icon, Theme, Window};
-            #[cfg(feature = "winrt")]
-            use windows_webview2::Windows::Win32::{Shell as shell, WindowsAndMessaging::HWND};
-            #[cfg(feature = "win32")]
-            use {
-                libc::c_void,
-                std::ptr,
-                winapi::{
-                    shared::windef::HWND,
-                    um::{
-                        combaseapi::{CoCreateInstance, CLSCTX_SERVER},
-                        shobjidl_core::{CLSID_TaskbarList, ITaskbarList},
-                    },
-                    DEFINE_GUID,
-                },
-            };
+      #[cfg(feature = "winrt")]
+      use windows_webview2::Windows::Win32::{Shell as shell, WindowsAndMessaging::HWND};
+      use winit::window::{Icon, Theme, Window};
+      #[cfg(feature = "win32")]
+      use {
+        libc::c_void,
+        std::ptr,
+        winapi::{
+          shared::windef::HWND,
+          um::{
+            combaseapi::{CoCreateInstance, CLSCTX_SERVER},
+            shobjidl_core::{CLSID_TaskbarList, ITaskbarList},
+          },
+          DEFINE_GUID,
+        },
+      };
 
-            /// Additional methods on `Window` that are specific to Windows.
-            pub trait WindowExtWindows {
-                /// Returns the HINSTANCE of the window
-                fn hinstance(&self) -> *mut libc::c_void;
-                /// Returns the native handle that is used by this window.
-                ///
-                /// The pointer will become invalid when the native window was destroyed.
-                fn hwnd(&self) -> *mut libc::c_void;
+      /// Additional methods on `Window` that are specific to Windows.
+      pub trait WindowExtWindows {
+        /// Returns the HINSTANCE of the window
+        fn hinstance(&self) -> *mut libc::c_void;
+        /// Returns the native handle that is used by this window.
+        ///
+        /// The pointer will become invalid when the native window was destroyed.
+        fn hwnd(&self) -> *mut libc::c_void;
 
-                /// This sets `ICON_BIG`. A good ceiling here is 256x256.
-                fn set_taskbar_icon(&self, taskbar_icon: Option<Icon>);
+        /// This sets `ICON_BIG`. A good ceiling here is 256x256.
+        fn set_taskbar_icon(&self, taskbar_icon: Option<Icon>);
 
-                /// This removes taskbar icon of the application.
-                fn skip_taskbar(&self);
+        /// This removes taskbar icon of the application.
+        fn skip_taskbar(&self);
 
-                /// Returns the current window theme.
-                fn theme(&self) -> Theme;
-            }
+        /// Returns the current window theme.
+        fn theme(&self) -> Theme;
+      }
 
-            impl WindowExtWindows for Window {
-                #[inline]
-                fn hinstance(&self) -> *mut libc::c_void {
-                    WindowExtWindows_::hinstance(self)
-                }
-
-                #[inline]
-                fn hwnd(&self) -> *mut libc::c_void {
-                    WindowExtWindows_::hwnd(self)
-                }
-
-                #[inline]
-                fn set_taskbar_icon(&self, taskbar_icon: Option<Icon>) {
-                    WindowExtWindows_::set_taskbar_icon(self, taskbar_icon)
-                }
-
-                #[inline]
-                fn skip_taskbar(&self) {
-                    #[cfg(feature = "winrt")]
-                        unsafe {
-                        if let Ok(taskbar_list) = windows::create_instance::<shell::ITaskbarList>(&shell::TaskbarList) {
-                            let _ = taskbar_list.DeleteTab(HWND(WindowExtWindows_::hwnd(self) as _));
-                        }
-                    }
-                    #[cfg(feature = "win32")]
-                        unsafe {
-                        let mut taskbar_list: *mut ITaskbarList = std::mem::zeroed();
-                        DEFINE_GUID! {IID_ITASKBAR_LIST, 0x56FDF342, 0xfd6d, 0x11d0, 0x95, 0x8a, 0x00, 0x60, 0x97, 0xc9, 0xa0, 0x90}
-                        CoCreateInstance(
-                            &CLSID_TaskbarList,
-                            ptr::null_mut(),
-                            CLSCTX_SERVER,
-                            &IID_ITASKBAR_LIST,
-                            &mut taskbar_list as *mut *mut ITaskbarList as *mut *mut c_void,
-                        );
-                        (*taskbar_list).DeleteTab(WindowExtWindows_::hwnd(self) as HWND);
-                        (*taskbar_list).Release();
-                    }
-                }
-
-                #[inline]
-                fn theme(&self) -> Theme {
-                    WindowExtWindows_::theme(self)
-                }
-            }
+      impl WindowExtWindows for Window {
+        #[inline]
+        fn hinstance(&self) -> *mut libc::c_void {
+          WindowExtWindows_::hinstance(self)
         }
+
+        #[inline]
+        fn hwnd(&self) -> *mut libc::c_void {
+          WindowExtWindows_::hwnd(self)
+        }
+
+        #[inline]
+        fn set_taskbar_icon(&self, taskbar_icon: Option<Icon>) {
+          WindowExtWindows_::set_taskbar_icon(self, taskbar_icon)
+        }
+
+        #[inline]
+        fn skip_taskbar(&self) {
+          #[cfg(feature = "winrt")]
+          unsafe {
+            if let Ok(taskbar_list) =
+              windows::create_instance::<shell::ITaskbarList>(&shell::TaskbarList)
+            {
+              let _ = taskbar_list.DeleteTab(HWND(WindowExtWindows_::hwnd(self) as _));
+            }
+          }
+          #[cfg(feature = "win32")]
+          unsafe {
+            let mut taskbar_list: *mut ITaskbarList = std::mem::zeroed();
+            DEFINE_GUID! {IID_ITASKBAR_LIST, 0x56FDF342, 0xfd6d, 0x11d0, 0x95, 0x8a, 0x00, 0x60, 0x97, 0xc9, 0xa0, 0x90}
+            CoCreateInstance(
+              &CLSID_TaskbarList,
+              ptr::null_mut(),
+              CLSCTX_SERVER,
+              &IID_ITASKBAR_LIST,
+              &mut taskbar_list as *mut *mut ITaskbarList as *mut *mut c_void,
+            );
+            (*taskbar_list).DeleteTab(WindowExtWindows_::hwnd(self) as HWND);
+            (*taskbar_list).Release();
+          }
+        }
+
+        #[inline]
+        fn theme(&self) -> Theme {
+          WindowExtWindows_::theme(self)
+        }
+      }
     }
+  }
 }
