0dbae3ffe30485649ae9d8c5ba9a21cc95535462
Ngo Iok Ui <wusyong9104@gmail.com>
2021-04-23 11:59:07 +0800

    Refactor application mod structure

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -2,10 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
+#[cfg(target_os = "linux")]
 pub use winit::dpi;
+#[cfg(target_os = "linux")]
 pub mod error;
+#[cfg(target_os = "linux")]
 pub mod event;
+#[cfg(target_os = "linux")]
 pub mod event_loop;
+#[cfg(target_os = "linux")]
 mod icon;
+#[cfg(target_os = "linux")]
 pub mod monitor;
+#[cfg(target_os = "linux")]
 pub mod window;
+
+#[cfg(not(target_os = "linux"))]
+pub use original::*;
+#[cfg(not(target_os = "linux"))]
+mod original {
+    pub use winit::*;
+}
