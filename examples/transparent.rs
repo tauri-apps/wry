e2785566c69d43f003896b7b5da79b29d2966c13
Ngo Iok Ui (Wu Yu Wei) <wusyong9104@gmail.com>
2021-04-28 11:47:19 +0800

    fix: transparency on Windows  (#217)
    * Fix transparency on Windows
    
    * Fix transparency on Windows
    
    * Add change file
    
    * Fix custom titlebar example

--- examples/transparent.rs
+++ examples/transparent.rs
@@ -15,17 +15,21 @@ fn main() -> wry::Result<()> {
   let event_loop = EventLoop::new();
   let window = WindowBuilder::new()
     .with_decorations(false)
+    // There are actually three layer of background color when creating webview window.
+    // The first is window background...
     .with_transparent(true)
     .build(&event_loop)
     .unwrap();
 
   let webview = WebViewBuilder::new(window)?
+    // The second is on webview...
     .with_transparent(true)
+    // And the last is in html.
     .with_url(
       r#"data:text/html,
             <!doctype html>
             <html>
-              <body style="background-color:rgba(87,87,87,0.);">hello</body>
+              <body style="background-color:rgba(87,87,87,0.5);">hello</body>
               <script>
                 window.onload = function() {
                   document.body.innerText = `hello, ${navigator.userAgent}`;
