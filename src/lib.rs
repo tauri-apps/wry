299a2b2e6e11f2765ce7c56669eb3c7036e1f884
Ngo Iok Ui <wusyong9104@gmail.com>
2021-05-16 15:18:15 +0800

    Update documentation about required packages

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@
 //! - `tray`: Enables system tray and more menu item variants on **Linux**. You can still create
 //! those types if you disable it. They just don't create the actual objects. We set this flag
 //! because some implementations require more installed packages. Disable this if you don't want
-//! to install `libappindicator` and `sourceview` package.
+//! to install `libappindicator`, `sourceview`, and `clang` package.
 //! - `menu`: Enables menu item variants on **Linux**. You can still create those types if you
 //! you disable it. They just don't create the actual objects. We set this flag  because some
 //! implementations require more installed packages. Disable this if you don't want to install
