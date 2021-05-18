8429af13a4d1ea1158e9533553c970779adf25da
David Lemarier <david@lemarier.ca>
2021-05-18 09:16:17 -0400

    add `cpu_intensive` benchmark

--- bench/main.rs
+++ bench/main.rs
@@ -26,8 +26,10 @@ fn write_json(filename: &str, value: &Value) -> Result<()> {
 }
 
 /// The list of the examples of the benchmark name, arguments and return code
-const EXEC_TIME_BENCHMARKS: &[(&str, &str, Option<i32>)] =
-  &[("hello_world", "target/release/bench_hello_world", None)];
+const EXEC_TIME_BENCHMARKS: &[(&str, &str, Option<i32>)] = &[
+  ("hello_world", "target/release/bench_hello_world", None),
+  ("cpu_intensive", "target/release/bench_cpu_intensive", None),
+];
 
 fn run_strace_benchmarks(new_data: &mut BenchResult) -> Result<()> {
   use std::io::Read;
