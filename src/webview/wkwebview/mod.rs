tauri-apps/wry

commit 0e2574c420f778c59bafc164ddee2bc0b7705ee9

Remove `&Window` parameter from Custom Protocol handlers (#361)
* support multiple WebViews in a single WebContext

* wait for load-change::finished

* add doc_cfg for errors

* update os specific cfgs

* cargo +nightly fmt

* Revert "update os specific cfgs"

This reverts commit a1f1776f48983a6641d5b26dbefeb40f2bccb908.

* Revert "add doc_cfg for errors"

This reverts commit efe7b0228d72bd9e187fa43ce44089d290ba8338.

* remove updated cfg values

* updated documentation

* change web_context refs to mut

* update changelog

* clippy

* clippy for win/macos

* update system tray example to track tao dev changes

* remove Window from custom protocol handlers

* add changefile

* blind fix macOS

Co-authored-by: Ngo Iok Ui <wusyong9104@gmail.com>

src/webview/wkwebview/mod.rs
@@ -52,10 +52,7 @@ pub struct InnerWebView {
   ),
   #[cfg(target_os = "macos")]
   file_drop_ptr: *mut (Box<dyn Fn(&Window, FileDropEvent) -> bool>, Rc<Window>),
-  protocol_ptrs: Vec<*mut (
-    Box<dyn Fn(&Window, &str) -> Result<(Vec<u8>, String)>>,
-    Rc<Window>,
-  )>,
+  protocol_ptrs: Vec<*mut Box<dyn Fn(&str) -> Result<(Vec<u8>, String)>>>,
 }
 
 impl InnerWebView {
@@ -163,8 +160,7 @@ impl InnerWebView {
           None => Class::get(&scheme_name).expect("Failed to get the class definition"),
         };
         let handler: id = msg_send![cls, new];
-        let w = window.clone();
-        let function = Box::into_raw(Box::new((function, w)));
+        let function = Box::into_raw(Box::new(function));
         protocol_ptrs.push(function);
 
         (*handler).set_ivar("function", function as *mut _ as *mut c_void);

src/webview/mod.rs
@@ -58,12 +58,9 @@ pub struct WebViewAttributes {
   /// Register custom file loading protocols with pairs of scheme uri string and a handling
   /// closure.
   ///
-  /// The closure takes the `Window` and a url string slice as parameters, and returns a tuple of a
-  /// vector of bytes which is the content and a mimetype string of the content.
-  pub custom_protocols: Vec<(
-    String,
-    Box<dyn Fn(&Window, &str) -> Result<(Vec<u8>, String)>>,
-  )>,
+  /// The closure takes a url string slice, and returns a two item tuple of a vector of
+  /// bytes which is the content and a mimetype string of the content.
+  pub custom_protocols: Vec<(String, Box<dyn Fn(&str) -> Result<(Vec<u8>, String)>>)>,
   /// Set the RPC handler to Communicate between the host Rust code and Javascript on webview.
   ///
   /// The communication is done via [JSON-RPC](https://www.jsonrpc.org). Users can use this to register an incoming
@@ -149,12 +146,12 @@ impl<'a> WebViewBuilder<'a> {
   /// Register custom file loading protocols with pairs of scheme uri string and a handling
   /// closure.
   ///
-  /// The closure takes the `Window` and a url string slice as parameters, and returns a tuple of a
+  /// The closure takes a url string slice, and returns a two item tuple of a
   /// vector of bytes which is the content and a mimetype string of the content.
   #[cfg(feature = "protocol")]
   pub fn with_custom_protocol<F>(mut self, name: String, handler: F) -> Self
   where
-    F: Fn(&Window, &str) -> Result<(Vec<u8>, String)> + 'static,
+    F: Fn(&str) -> Result<(Vec<u8>, String)> + 'static,
   {
     self
       .webview

src/webview/webkitgtk/mod.rs
@@ -171,7 +171,7 @@ impl InnerWebView {
 
     // File drop handling
     if let Some(file_drop_handler) = attributes.file_drop_handler {
-      file_drop::connect_drag_event(webview.clone(), window_rc.clone(), file_drop_handler);
+      file_drop::connect_drag_event(webview.clone(), window_rc, file_drop_handler);
     }
 
     if window.get_visible() {
@@ -193,8 +193,7 @@ impl InnerWebView {
     }
 
     for (name, handler) in attributes.custom_protocols {
-      let w = window_rc.clone();
-      if let Err(e) = web_context.register_uri_scheme(&name, handler, w) {
+      if let Err(e) = web_context.register_uri_scheme(&name, handler) {
         if let Error::DuplicateCustomProtocol(_) = e {
           // Swallow duplicate scheme errors to preserve current behavior.
           // FIXME: we should log this error in the future

src/webview/webview2/mod.rs
@@ -196,7 +196,6 @@ impl InnerWebView {
 
           let custom_protocols = attributes.custom_protocols;
           let env_clone = env_.clone();
-          let window_ = window.clone();
           w.add_web_resource_requested(move |_, args| {
             let uri = args.get_request()?.get_uri()?;
             // Undo the protocol workaround when giving path to resolver
@@ -209,7 +208,7 @@ impl InnerWebView {
               .iter()
               .find(|(name, _)| name == &scheme)
               .unwrap()
-              .1)(&window_, &path)
+              .1)(&path)
             {
               Ok((content, mime)) => {
                 let stream = webview2::Stream::from_bytes(&content);

bench/tests/src/cpu_intensive.rs
@@ -25,7 +25,7 @@ fn main() -> wry::Result<()> {
   };
   let webview = WebViewBuilder::new(window)
     .unwrap()
-    .with_custom_protocol("wry.bench".into(), move |_, requested_asset_path| {
+    .with_custom_protocol("wry.bench".into(), move |requested_asset_path| {
       let requested_asset_path = requested_asset_path.replace("wry.bench://", "");
       match requested_asset_path.as_str() {
         "/index.css" => Ok((include_bytes!("static/index.css").to_vec(), "text/css".into())),

bench/tests/src/custom_protocol.rs
@@ -32,7 +32,7 @@ fn main() -> wry::Result<()> {
   let webview = WebViewBuilder::new(window)
     .unwrap()
     .with_rpc_handler(handler)
-    .with_custom_protocol("wry.bench".into(), move |_, _| {
+    .with_custom_protocol("wry.bench".into(), move |_uri| {
       let index_html = r#"
       <!DOCTYPE html>
       <html lang="en">

examples/custom_protocol.rs
@@ -22,7 +22,7 @@ fn main() -> wry::Result<()> {
 
   let _webview = WebViewBuilder::new(window)
     .unwrap()
-    .with_custom_protocol("wry".into(), move |_, requested_asset_path| {
+    .with_custom_protocol("wry".into(), move |requested_asset_path| {
       // Remove url scheme
       let path = requested_asset_path.replace("wry://", "");
       // Read the file content from file path

examples/system_tray.rs
@@ -129,7 +129,7 @@ fn main() -> wry::Result<()> {
 
       let webview = WebViewBuilder::new(window)
         .unwrap()
-        .with_custom_protocol("wry.dev".into(), move |_, _| {
+        .with_custom_protocol("wry.dev".into(), move |_uri| {
           Ok((index_html.as_bytes().into(), "text/html".into()))
         })
         .with_url("wry.dev://")

commit ddb755964bdc5933494e7155eb8d27d88b58d279

fix macos custom protocol bug introduced in #361

src/webview/wkwebview/mod.rs
@@ -95,11 +95,8 @@ impl InnerWebView {
     extern "C" fn start_task(this: &Object, _: Sel, _webview: id, task: id) {
       unsafe {
         let function = this.get_ivar::<*mut c_void>("function");
-        let function = &mut *(*function
-          as *mut (
-            Box<dyn for<'r, 's> Fn(&'r Window, &'s str) -> Result<(Vec<u8>, String)>>,
-            Rc<Window>,
-          ));
+        let function =
+          &mut *(*function as *mut Box<dyn for<'s> Fn(&'s str) -> Result<(Vec<u8>, String)>>);
 
         // Get url request
         let request: id = msg_send![task, request];
@@ -111,7 +108,7 @@ impl InnerWebView {
         let uri = nsstring.to_str();
 
         // Send response
-        if let Ok((content, mime)) = function.0(&function.1, uri) {
+        if let Ok((content, mime)) = function(uri) {
           let dictionary: id = msg_send![class!(NSMutableDictionary), alloc];
           let headers: id = msg_send![dictionary, initWithCapacity:1];
           let () = msg_send![headers, setObject:NSString::new(&mime) forKey: NSString::new("content-type")];