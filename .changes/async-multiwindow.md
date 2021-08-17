1145d50937f0114eefccc9b7722fc2be013cd454
Chip Reed <chip@chip.sh>
2021-08-16 21:45:21 -0700

    fix async multiwindow deadlock

---
+++ .changes/async-multiwindow.md
@@ -0,0 +1,5 @@
+---
+"wry": patch
+---
+
+Fixed a Linux multi-window issue where the internal url loader didn't unlock when flushed while empty
--- src/webview/web_context.rs
+++ src/webview/web_context.rs
@@ -404,6 +404,8 @@ pub mod unix {
           });
 
           webview.load_uri(url.as_str());
+        } else {
+            self.unlock();
         }
       }
     }
