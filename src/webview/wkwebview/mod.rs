26d38810b82d8176936de7484341e4a10d45899b
keiya sasaki <34934510+keiya01@users.noreply.github.com>
2022-08-01 20:17:47 +0900

    fix: remove pip runtime feature flag

--- src/webview/wkwebview/mod.rs
+++ src/webview/wkwebview/mod.rs
@@ -261,7 +261,6 @@ impl InnerWebView {
         let _: id = msg_send![_preference, setValue:_yes forKey:dev];
       }
 
-      #[cfg(feature = "pictureInPicture")]
       let _: id = msg_send![_preference, setValue:_yes forKey:NSString::new("allowsPictureInPictureMediaPlayback")];
 
       #[cfg(target_os = "macos")]
