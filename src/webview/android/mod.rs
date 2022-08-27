917bb4309a0b027c038f2bcfd338683acb2252e8
Amr Bashir <amr.bashir2015@gmail.com>
2022-08-27 04:07:03 +0200

    chore: licenses (#675)

--- src/webview/android/mod.rs
+++ src/webview/android/mod.rs
@@ -1,3 +1,7 @@
+// Copyright 2020-2022 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use super::{WebContext, WebViewAttributes};
 use crate::{
   application::window::Window,
