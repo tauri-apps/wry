tauri-apps/wry

commit 51b49c54e41c71d1c5f03b568094d43fb9dc32ac

feat(webview2): hide the webview when the window is minimized (#702)
* feat(webview2): hide the webview when the window is minimized

* update covector readme

src/webview/webview2/mod.rs
@@ -614,6 +614,14 @@ window.addEventListener('mousemove', (e) => window.chrome.webview.postMessage('_
             right: client_rect.right - client_rect.left,
             bottom: client_rect.bottom - client_rect.top,
           });
+
+          if wparam == WPARAM(win32wm::SIZE_MINIMIZED as _) {
+            let _ = (*controller).SetIsVisible(false);
+          }
+
+          if wparam == WPARAM(win32wm::SIZE_RESTORED as _) {
+            let _ = (*controller).SetIsVisible(true);
+          }
         }
 
         win32wm::WM_SETFOCUS | win32wm::WM_ENTERSIZEMOVE => {

commit 541e606a304d862c12c7c5ee814a3d21fcef2094

fix: restore webview2 visibility upon restoring a maximized window
regression was introduce in https://github.com/tauri-apps/wry/pull/702

src/webview/webview2/mod.rs
@@ -698,7 +698,9 @@ window.addEventListener('mousemove', (e) => window.chrome.webview.postMessage('_
             let _ = (*controller).SetIsVisible(false);
           }
 
-          if wparam == WPARAM(win32wm::SIZE_RESTORED as _) {
+          if wparam == WPARAM(win32wm::SIZE_RESTORED as _)
+            || wparam == WPARAM(win32wm::SIZE_MAXIMIZED as _)
+          {
             let _ = (*controller).SetIsVisible(true);
           }
         }