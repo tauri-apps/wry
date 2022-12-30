dfe6a5e78acca05d9e0808c8f4ed974a8657b847
David Craven <david@craven.ch>
2022-12-30 02:02:46 +0100

    refactor: improve android env vars naming (#829)
    * Improve naming.
    
    * change file

--- build.rs
+++ build.rs
@@ -23,13 +23,13 @@ fn main() {
       })
     }
 
-    println!("cargo:rerun-if-env-changed=WRY_ANDROID_REVERSED_DOMAIN");
-    println!("cargo:rerun-if-env-changed=WRY_ANDROID_APP_NAME_SNAKE_CASE");
+    println!("cargo:rerun-if-env-changed=WRY_ANDROID_PACKAGE");
+    println!("cargo:rerun-if-env-changed=WRY_ANDROID_LIBRARY");
     println!("cargo:rerun-if-env-changed=WRY_ANDROID_KOTLIN_FILES_OUT_DIR");
 
     if let Ok(kotlin_out_dir) = std::env::var("WRY_ANDROID_KOTLIN_FILES_OUT_DIR") {
-      let reversed_domain = env_var("WRY_ANDROID_REVERSED_DOMAIN");
-      let app_name_snake_case = env_var("WRY_ANDROID_APP_NAME_SNAKE_CASE");
+      let package = env_var("WRY_ANDROID_PACKAGE");
+      let library = env_var("WRY_ANDROID_LIBRARY");
 
       let kotlin_out_dir = PathBuf::from(&kotlin_out_dir)
         .canonicalize()
@@ -69,8 +69,8 @@ fn main() {
 
         let content = fs::read_to_string(file.path())
           .expect("failed to read kotlin file as string")
-          .replace("{{app-domain-reversed}}", &reversed_domain)
-          .replace("{{app-name-snake-case}}", &app_name_snake_case)
+          .replace("{{package}}", &package)
+          .replace("{{library}}", &library)
           .replace(
             "{{class-extension}}",
             &std::env::var(&class_extension_env).unwrap_or_default(),
