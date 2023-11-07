35c2b58e2ffca4687519480862d122472c008439
Jason Tsai <jason@pews.dev>
2023-11-07 13:58:40 +0800

    chore: add load_html example

---
+++ examples/load_html.rs
@@ -0,0 +1,67 @@
+// Copyright 2020-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+fn main() -> wry::Result<()> {
+  use wry::{
+    application::{
+      event::{Event, StartCause, WindowEvent},
+      event_loop::{ControlFlow, EventLoopBuilder},
+      window::{Window, WindowBuilder},
+    },
+    webview::WebViewBuilder,
+  };
+
+  enum UserEvent {
+    LoadHtml,
+  }
+
+  let event_loop = EventLoopBuilder::<UserEvent>::with_user_event().build();
+  let proxy = event_loop.create_proxy();
+
+  let window = WindowBuilder::new()
+    .with_title("Load HTML")
+    .build(&event_loop)?;
+
+  let ipc_handler = move |_: &Window, req: String| {
+    if req == "load-html" {
+      let _ = proxy.send_event(UserEvent::LoadHtml);
+    }
+  };
+
+  let webview = WebViewBuilder::new(window)?
+    .with_html(
+      r#"
+      <button onclick="window.ipc.postMessage('load-html')">Load HTML</button>
+    "#,
+    )?
+    .with_ipc_handler(ipc_handler)
+    .build()?;
+
+  const HTML: &str = r#"
+  <html>
+  <body>
+      <h1> HTML LOADED </h1>
+  </body>
+  <script>
+    alert("HTML Loaded");
+  </script>
+  </html>
+"#;
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+      Event::UserEvent(UserEvent::LoadHtml) => {
+        webview.load_html(HTML);
+      }
+      Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => *control_flow = ControlFlow::Exit,
+      _ => (),
+    }
+  });
+}
