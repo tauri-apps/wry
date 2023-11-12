d2f42203d138537ac2036a24f2dd50ab3c81bdee
Lucas Fernandes Nogueira <lucas@tauri.studio>
2023-11-12 11:27:36 -0300

    fix(android): properly use $wry on android_binding macro (#1075)
    * fix(android): properly `use $wry` on android_binding macro
    
    The android_binding macro fails to compile due to the `use $wry::prelude::*;` with `expected { or *` error.
    
    Regression from #1041 which was not released yet so I didn't add a change file.
    
    * use {{ }}

--- src/android/binding.rs
+++ src/android/binding.rs
@@ -27,8 +27,10 @@ macro_rules! android_binding {
   ($domain:ident, $package:ident) => {
     ::wry::android_binding!($domain, $package, ::wry)
   };
+  // use import `android_setup` just to force the import path to use `wry::{}`
+  // as the macro breaks without braces
   ($domain:ident, $package:ident, $wry:path) => {{
-    use $wry::prelude::*;
+    use $wry::{android_setup as _, prelude::*};
 
     android_fn!(
       $domain,
