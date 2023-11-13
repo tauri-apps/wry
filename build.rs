474919fbe78a03136e5a720d2c092df18eb96783
Wu Yu Wei <yuweiwu@pm.me>
2023-11-13 19:11:23 +0900

    Add native feature flag to public

--- build.rs
+++ build.rs
@@ -110,6 +110,6 @@ fn main() {
       apple: { any(target_os = "ios", target_os = "macos") },
       linux: { all(unix, not(apple), not(android)) },
       // Backends
-      gtk: { all(feature = "gtk", linux) },
+      gtk: { all(feature = "native", linux) },
   }
 }
