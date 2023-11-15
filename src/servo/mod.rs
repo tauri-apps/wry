3e3f4682f2f5ca4a705af29d5ef142802758df6a
Wu Yu Wei <yuweiwu@pm.me>
2023-11-15 15:06:11 +0900

    Add window and servo thread

--- src/servo/mod.rs
+++ src/servo/mod.rs
@@ -1,13 +1,22 @@
+use crossbeam_channel::Sender;
 use raw_window_handle::HasRawWindowHandle;
 use url::Url;
 
 use crate::{Result, WebContext, WebViewAttributes, RGBA};
 
+use self::{
+  embedder::{ServoEvent, SERVO},
+  window::Window,
+};
+
 mod embedder;
 mod prefs;
 mod resources;
+mod window;
 
-pub(crate) struct InnerWebView;
+pub(crate) struct InnerWebView {
+  embedder_tx: Sender<ServoEvent>,
+}
 
 impl InnerWebView {
   pub fn new<W: HasRawWindowHandle>(
@@ -18,7 +27,13 @@ impl InnerWebView {
   ) -> Result<Self> {
     resources::init(web_context);
     prefs::init();
-    Ok(Self)
+
+    let embedder_tx = SERVO.sender();
+    embedder_tx
+      .send(ServoEvent::NewWebView(window.raw_window_handle()))
+      .expect("Fail to send event to Servo thread.");
+
+    Ok(Self { embedder_tx })
   }
 
   pub fn new_as_child<W: HasRawWindowHandle>(
@@ -27,7 +42,8 @@ impl InnerWebView {
     pl_attrs: super::PlatformSpecificWebViewAttributes,
     web_context: Option<&mut WebContext>,
   ) -> Result<Self> {
-    Ok(Self)
+    // Ok(Self)
+    todo!()
   }
 
   pub fn print(&self) {}
