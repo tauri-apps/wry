ca77200b9627c152ccb99ab91d09a83a68b705ab
Jason Tsai <jason@pews.dev>
2023-12-13 16:46:15 +0800

    fix(servo): update servo client to vendor's new constructor interface

--- src/servo/embedder.rs
+++ src/servo/embedder.rs
@@ -40,6 +40,7 @@ impl Embedder {
       Some(String::from(
         "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0",
       )),
+      servo::compositing::CompositeTarget::Window,
     );
 
     init_servo
