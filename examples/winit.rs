f93d9bf21ade575e5f0a0dc0547c09fa37af59c4
Jason Tsai <jason@pews.dev>
2023-12-28 14:34:55 +0900

    window decoration

--- examples/winit.rs
+++ examples/winit.rs
@@ -10,39 +10,31 @@ use winit::{
 };
 use wry::{WebViewBuilder, WebViewBuilderExtServo, WebViewExtServo};
 
+/* window decoration */
+use cocoa::appkit::{NSView, NSWindow};
+use cocoa::appkit::{NSWindowStyleMask, NSWindowTitleVisibility};
+use objc::{msg_send, runtime::Object, sel, sel_impl};
+use raw_window_handle::{AppKitWindowHandle, HasRawWindowHandle, RawWindowHandle};
+use winit::dpi::LogicalPosition;
+use winit::platform::macos::WindowBuilderExtMacOS;
+
 fn main() -> wry::Result<()> {
-  // #[cfg(any(
-  //   target_os = "linux",
-  //   target_os = "dragonfly",
-  //   target_os = "freebsd",
-  //   target_os = "netbsd",
-  //   target_os = "openbsd",
-  // ))]
-  // {
-  //   use gtk::prelude::DisplayExtManual;
-
-  //   gtk::init().unwrap();
-  //   if gtk::gdk::Display::default().unwrap().backend().is_wayland() {
-  //     panic!("This example doesn't support wayland!");
-  //   }
-
-  //   // we need to ignore this error here otherwise it will be catched by winit and will be
-  //   // make the example crash
-  //   winit::platform::x11::register_xlib_error_hook(Box::new(|_display, error| {
-  //     let error = error as *mut x11_dl::xlib::XErrorEvent;
-  //     (unsafe { (*error).error_code }) == 170
-  //   }));
-  // }
-
   let event_loop = EventLoop::new().unwrap();
   let window = WindowBuilder::new()
-    .with_inner_size(PhysicalSize::new(1000, 800))
+    .with_inner_size(PhysicalSize::new(1000, 500))
     .build(&event_loop)
     .unwrap();
 
+  unsafe {
+    let rwh = window.raw_window_handle();
+    if let RawWindowHandle::AppKit(AppKitWindowHandle { ns_window, .. }) = rwh {
+      decorate_window(ns_window as *mut Object, LogicalPosition::new(8.0, 40.0));
+    }
+  }
+
   #[allow(unused_mut)]
   let mut builder = WebViewBuilder::new_servo(window, event_loop.create_proxy());
-  let mut webview = builder.with_url("https://tauri.app")?.build()?;
+  let mut webview = builder.build()?;
 
   event_loop
     .run(move |event, evl| {
@@ -60,4 +52,17 @@ fn main() -> wry::Result<()> {
     .unwrap();
 
   Ok(())
+}
+
+pub unsafe fn decorate_window(window: *mut Object, position: LogicalPosition<f64>) {
+  NSWindow::setTitlebarAppearsTransparent_(window, true);
+  NSWindow::setTitleVisibility_(window, NSWindowTitleVisibility::NSWindowTitleHidden);
+  NSWindow::setStyleMask_(
+    window,
+    NSWindowStyleMask::NSTitledWindowMask
+      | NSWindowStyleMask::NSFullSizeContentViewWindowMask
+      | NSWindowStyleMask::NSClosableWindowMask
+      | NSWindowStyleMask::NSResizableWindowMask
+      | NSWindowStyleMask::NSMiniaturizableWindowMask,
+  );
 }
