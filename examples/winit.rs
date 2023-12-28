c0edab51c3357adbe46b1f3c06b5e165f8a5fb5b
Jason Tsai <jason@pews.dev>
2023-12-28 14:34:55 +0900

    window decoration

--- examples/winit.rs
+++ examples/winit.rs
@@ -1,100 +1,68 @@
-use dpi::{LogicalPosition, LogicalSize};
+// Copyright 2020-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use winit::{
-  application::ApplicationHandler,
-  event::WindowEvent,
-  event_loop::{ActiveEventLoop, EventLoop},
-  window::{Window, WindowId},
+  dpi::PhysicalSize,
+  event::{Event, WindowEvent},
+  event_loop::{ControlFlow, EventLoop},
+  window::WindowBuilder,
 };
-use wry::{Rect, WebViewBuilder};
+use wry::{WebViewBuilder, WebViewBuilderExtServo, WebViewExtServo};
 
-#[derive(Default)]
-struct State {
-  window: Option<Window>,
-  webview: Option<wry::WebView>,
-}
-
-impl ApplicationHandler for State {
-  fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-    let mut attributes = Window::default_attributes();
-    attributes.inner_size = Some(LogicalSize::new(800, 800).into());
-    let window = event_loop.create_window(attributes).unwrap();
-
-    let webview = WebViewBuilder::new()
-      .with_url("https://tauri.app")
-      .build_as_child(&window)
-      .unwrap();
-
-    self.window = Some(window);
-    self.webview = Some(webview);
-  }
-
-  fn window_event(
-    &mut self,
-    _event_loop: &ActiveEventLoop,
-    _window_id: WindowId,
-    event: WindowEvent,
-  ) {
-    match event {
-      WindowEvent::Resized(size) => {
-        let window = self.window.as_ref().unwrap();
-        let webview = self.webview.as_ref().unwrap();
-
-        let size = size.to_logical::<u32>(window.scale_factor());
-        webview
-          .set_bounds(Rect {
-            position: LogicalPosition::new(0, 0).into(),
-            size: LogicalSize::new(size.width, size.height).into(),
-          })
-          .unwrap();
-      }
-      WindowEvent::CloseRequested => {
-        std::process::exit(0);
-      }
-      _ => {}
-    }
-  }
-
-  fn about_to_wait(&mut self, _event_loop: &ActiveEventLoop) {
-    #[cfg(any(
-      target_os = "linux",
-      target_os = "dragonfly",
-      target_os = "freebsd",
-      target_os = "netbsd",
-      target_os = "openbsd",
-    ))]
-    {
-      while gtk::events_pending() {
-        gtk::main_iteration_do(false);
-      }
-    }
-  }
-}
+/* window decoration */
+use cocoa::appkit::{NSView, NSWindow};
+use cocoa::appkit::{NSWindowStyleMask, NSWindowTitleVisibility};
+use objc::{msg_send, runtime::Object, sel, sel_impl};
+use raw_window_handle::{AppKitWindowHandle, HasRawWindowHandle, RawWindowHandle};
+use winit::dpi::LogicalPosition;
+use winit::platform::macos::WindowBuilderExtMacOS;
 
 fn main() -> wry::Result<()> {
-  #[cfg(any(
-    target_os = "linux",
-    target_os = "dragonfly",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "openbsd",
-  ))]
-  {
-    use gtk::prelude::DisplayExtManual;
+  let event_loop = EventLoop::new().unwrap();
+  let window = WindowBuilder::new()
+    .with_inner_size(PhysicalSize::new(1000, 500))
+    .build(&event_loop)
+    .unwrap();
 
-    gtk::init().unwrap();
-    if gtk::gdk::Display::default().unwrap().backend().is_wayland() {
-      panic!("This example doesn't support wayland!");
+  unsafe {
+    let rwh = window.raw_window_handle();
+    if let RawWindowHandle::AppKit(AppKitWindowHandle { ns_window, .. }) = rwh {
+      decorate_window(ns_window as *mut Object, LogicalPosition::new(8.0, 40.0));
     }
-
-    winit::platform::x11::register_xlib_error_hook(Box::new(|_display, error| {
-      let error = error as *mut x11_dl::xlib::XErrorEvent;
-      (unsafe { (*error).error_code }) == 170
-    }));
   }
 
-  let event_loop = EventLoop::new().unwrap();
-  let mut state = State::default();
-  event_loop.run_app(&mut state).unwrap();
+  #[allow(unused_mut)]
+  let mut builder = WebViewBuilder::new_servo(window, event_loop.create_proxy());
+  let mut webview = builder.build()?;
+
+  event_loop
+    .run(move |event, evl| {
+      if !evl.exiting() && webview.servo().is_shutdown() {
+        if let Some(servo) = webview.servo().servo_client().take() {
+          servo.deinit();
+        }
+        evl.exit();
+      } else {
+        webview.servo().set_control_flow(&event, evl);
+        webview.servo().handle_winit_event(event);
+        webview.servo().handle_servo_messages();
+      }
+    })
+    .unwrap();
 
   Ok(())
+}
+
+pub unsafe fn decorate_window(window: *mut Object, position: LogicalPosition<f64>) {
+  NSWindow::setTitlebarAppearsTransparent_(window, true);
+  NSWindow::setTitleVisibility_(window, NSWindowTitleVisibility::NSWindowTitleHidden);
+  NSWindow::setStyleMask_(
+    window,
+    NSWindowStyleMask::NSTitledWindowMask
+      | NSWindowStyleMask::NSFullSizeContentViewWindowMask
+      | NSWindowStyleMask::NSClosableWindowMask
+      | NSWindowStyleMask::NSResizableWindowMask
+      | NSWindowStyleMask::NSMiniaturizableWindowMask,
+  );
 }
