7a2ba931ac15fefed08c898a4abf7ce6265ba6f5
rhysd <lin90162@yahoo.co.jp>
2024-01-23 23:21:34 +0900

    fix(macos): fix SEGV when calling `WebView::evaluate_javascript` on release build
    Signed-off-by: rhysd <lin90162@yahoo.co.jp>

---
+++ .changes/fix-eval-crash.md
@@ -0,0 +1,5 @@
+---
+"wry": patch
+---
+
+On macOS, fix a release build crashes with SEGV when calling `WebView::evaluate_javascript`.
--- src/wkwebview/mod.rs
+++ src/wkwebview/mod.rs
@@ -956,7 +956,7 @@ r#"Object.defineProperty(window, 'ipc', {
         let span = Mutex::new(Some(tracing::debug_span!("wry::eval").entered()));
 
         // we need to check if the callback exists outside the handler otherwise it's a segfault
-        if let Some(callback) = &callback {
+        if let Some(callback) = callback {
           let handler = block::ConcreteBlock::new(move |val: id, _err: id| {
             #[cfg(feature = "tracing")]
             span.lock().unwrap().take();
@@ -972,15 +972,18 @@ r#"Object.defineProperty(window, 'ipc', {
             }
 
             callback(result);
-          });
+          }).copy();
 
           let _: () =
             msg_send![self.webview, evaluateJavaScript:NSString::new(js) completionHandler:handler];
         } else {
+          #[cfg(feature = "tracing")]
           let handler = block::ConcreteBlock::new(move |_val: id, _err: id| {
-            #[cfg(feature = "tracing")]
             span.lock().unwrap().take();
-          });
+          })
+          .copy();
+          #[cfg(not(feature = "tracing"))]
+          let handler = null::<*const c_void>();
 
           let _: () =
             msg_send![self.webview, evaluateJavaScript:NSString::new(js) completionHandler:handler];
