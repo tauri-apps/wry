c6ab4bcd88f09286273bb8b513fa7301d4679457
Lucas Nogueira <lucas@tauri.studio>
2024-03-06 10:37:35 -0300

    docs

--- src/android/kotlin/Ipc.kt
+++ src/android/kotlin/Ipc.kt
@@ -12,6 +12,9 @@ class Ipc(val webViewClient: RustWebViewClient) {
     @JavascriptInterface
     fun postMessage(message: String?) {
         message?.let {m ->
+            // we're not using WebView::getUrl() here because it needs to be executed on the main thread
+            // and it would slow down the Ipc
+            // so instead we track the current URL on the webview client
             this.ipc(webViewClient.currentUrl, m)
         }
     }
