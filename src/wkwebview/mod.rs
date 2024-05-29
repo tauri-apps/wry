b6e93ebb83b206b9d7b0470af1731f7f76c5179c
Eugene Hauptmann <eugene@reactivelions.com>
2024-05-29 19:34:43 -0400

    added custom_data_store_available to visionos

--- src/wkwebview/mod.rs
+++ src/wkwebview/mod.rs
@@ -358,6 +358,9 @@ impl InnerWebView {
 
       #[cfg(target_os = "ios")]
       let custom_data_store_available = os_version.0 >= 17;
+      
+      #[cfg(target_os = "visionos")]
+      let custom_data_store_available = os_version.0 >= 1;
 
       let data_store: id = match (
         attributes.incognito,
