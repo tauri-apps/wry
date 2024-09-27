323623d30ec34e40cccd9572f4910a2840745823
D.A.Pelasgus <demetrius@pelasgus.xyz>
2024-09-27 22:38:38 +0300

    Guix
    Missing package to be added

---
+++ dev-shells/manifest.scm
@@ -0,0 +1,8 @@
+;; manifest.scm
+;; Author: D.A.Pelasgus
+
+(specifications->manifest
+  '("pkg-config"                ; Helper tool used when compiling
+    "webkitgtk"                 ; Web content engine fot GTK+
+    "libappindicator"            ; Menu in Menu bar
+ ))
