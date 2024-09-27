7cd7a4a56811ddd18881a4714fdb55c11114a716
D.A.Pelasgus <demetrius@pelasgus.xyz>
2024-09-28 01:10:08 +0300

    Document & Dev Shells
    Added a missing package for guix dev. Formatted the readme using useful eyecatchers based on GitHub's latest markdown features.

--- dev-shells/manifest.scm
+++ dev-shells/manifest.scm
@@ -5,4 +5,5 @@
   '("pkg-config"                ; Helper tool used when compiling
     "webkitgtk"                 ; Web content engine fot GTK+
     "libappindicator"            ; Menu in Menu bar
+    "gtk"
  ))
