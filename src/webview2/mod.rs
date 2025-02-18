854874f41936258025f600929f13ce41e325a7ec
Neil McGuire <neil.mcguire@grammarly.com>
2025-02-18 15:01:14 -0500

    Enable custom protocols for all Source Kinds

--- src/webview2/mod.rs
+++ src/webview2/mod.rs
@@ -843,7 +843,16 @@ impl InnerWebView {
       // WebView2 supports non-standard protocols only on Windows 10+, so we have to use this workaround
       // See https://github.com/MicrosoftEdge/WebView2Feedback/issues/73
       let filter = HSTRING::from(format!("{scheme}://{name}.*"));
-      webview.AddWebResourceRequestedFilter(&filter, COREWEBVIEW2_WEB_RESOURCE_CONTEXT_ALL)?;
+
+      // If WebView2 version is high enough, use the new API to add the filter to allow Shared Workers and 
+      // iframes to work with custom protocols
+      // See https://github.com/MicrosoftEdge/WebView2Feedback/issues/1114
+      if let Ok(webview_22) = webview.cast::<ICoreWebView2_22>() {
+        webview_22.AddWebResourceRequestedFilterWithRequestSourceKinds(&filter, COREWEBVIEW2_WEB_RESOURCE_CONTEXT_ALL, COREWEBVIEW2_WEB_RESOURCE_REQUEST_SOURCE_KINDS_ALL)?;
+      } else {
+        // Fallback to the old API
+        webview.AddWebResourceRequestedFilter(&filter, COREWEBVIEW2_WEB_RESOURCE_CONTEXT_ALL)?;
+      }
     }
 
     let env = env.clone();
