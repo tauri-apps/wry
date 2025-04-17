74312c026f26b948d9782b67294ed2646c8b658c
Youn Mélois <youn@melois.dev>
2025-04-17 09:58:39 +0200

    fix: cache_directory cannot be set through setter
    If that was the case, the `os` property would be re-set with the new
    value for the cache directory.
    
    Also, I mis-swapped `data_directory` and `cache_directory` in
    `WebContextImpl::new`'s call.

--- src/web_context.rs
+++ src/web_context.rs
@@ -34,13 +34,17 @@ pub struct WebContext {
 impl WebContext {
   /// Create a new [`WebContext`].
   ///
+  /// `cache_directory`:
+  /// * Whether the WebView window should have a custom cache path. This is useful in Windows
+  ///   when a bundled application can't have the webview cache inside `Program Files`.
+  ///
   /// `data_directory`:
   /// * Whether the WebView window should have a custom user data path. This is useful in Windows
   ///   when a bundled application can't have the webview data inside `Program Files`.
-  pub fn new(data_directory: Option<PathBuf>) -> Self {
+  pub fn new(cache_directory: Option<PathBuf>, data_directory: Option<PathBuf>) -> Self {
     Self {
-      cache_directory: None,
-      os: WebContextImpl::new(data_directory.as_deref(), None),
+      os: WebContextImpl::new(cache_directory.as_deref(), data_directory.as_deref()),
+      cache_directory,
       data_directory,
       custom_protocols: Default::default(),
     }
@@ -64,11 +68,6 @@ impl WebContext {
   /// A reference to the data directory the context was created with.
   pub fn data_directory(&self) -> Option<&Path> {
     self.data_directory.as_deref()
-  }
-
-  /// Set the cache directory for the context.
-  pub fn set_cache_directory(&mut self, cache_directory: PathBuf) {
-    self.cache_directory = Some(cache_directory);
   }
 
   #[allow(dead_code)]
@@ -96,7 +95,7 @@ impl WebContext {
 
 impl Default for WebContext {
   fn default() -> Self {
-    Self::new(None)
+    Self::new(None, None)
   }
 }
 
