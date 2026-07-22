204b5d63ee6264ffa5c91de9949424adce7a930e
FabianLars <30730186+FabianLars@users.noreply.github.com>
2026-07-22 14:40:58 +0200

    don't rely on deployment target like i actually intended to do it

--- build.rs
+++ build.rs
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
+#[cfg(target_os = "macos")]
+#[path = "src/wkwebview/util.rs"]
+mod util;
+
 fn main() {
   let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
   if target_os == "macos" || target_os == "ios" {
@@ -9,11 +13,14 @@ fn main() {
   }
 
   // TODO: Remove in objc2 0.7.0, ref https://github.com/madsmtm/objc2/issues/645
-  if target_os == "macos"
-    && std::env::var("CARGO_CFG_DEBUG_ASSERTIONS").is_ok()
-    && !objc2::available!(macos = 12.0)
+  #[cfg(target_os = "macos")]
   {
-    println!("cargo:rustc-cfg=macos_12_3_unavailable")
+    if std::env::var("CARGO_CFG_DEBUG_ASSERTIONS").is_ok() {
+      let os = util::operating_system_version();
+      if os.0 < 12 {
+        println!("cargo:rustc-cfg=macos_12_unavailable")
+      }
+    }
   }
 
   if target_os == "android" {
