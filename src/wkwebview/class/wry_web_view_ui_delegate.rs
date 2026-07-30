aff28ef5a48e717e1b14b716e452124663cc4df1
Fabian-Lars <30730186+FabianLars@users.noreply.github.com>
2026-07-30 12:53:57 +0200

    fix(macos): dont register unknown delegate methods in debug mode (#1781)

--- src/wkwebview/class/wry_web_view_ui_delegate.rs
+++ src/wkwebview/class/wry_web_view_ui_delegate.rs
@@ -124,6 +124,9 @@ define_class!(
       }
     }
 
+    // TODO: Remove in objc2 0.7.0, ref <https://github.com/madsmtm/objc2/issues/645>
+    // Using not(unavailable) instead of available to prevent false positives if the build script failed to set the cfg
+    #[cfg(not(macos_12_unavailable))]
     #[unsafe(method(webView:requestMediaCapturePermissionForOrigin:initiatedByFrame:type:decisionHandler:))]
     fn request_media_capture_permission(
       &self,
