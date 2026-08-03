624893a06460a7d6f5b7b7da670fbcadd69533ea
Tony <legendmastertony@gmail.com>
2026-08-03 15:10:01 +0800

    add link to the config change docs

--- src/android/mod.rs
+++ src/android/mod.rs
@@ -141,6 +141,7 @@ pub unsafe fn android_setup(
 
   // We don't always destroy the rust window when `onDestroy` if it's caused by a configuration change.
   // This is used to re-create the webview to match in that case.
+  // See https://developer.android.com/guide/topics/resources/runtime-changes
   if let Some(webview_attributes) = WEBVIEW_ATTRIBUTES.lock().unwrap().get(&activity_id) {
     MainPipe::send(
       activity_id,
